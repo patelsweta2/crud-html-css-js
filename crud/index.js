9a29b6e95f0bccd9673360eb512b9db37fa4be47
Sweta Patel <sweta0442@gmail.com>
2025-03-27 21:00:07 +0530

    first machine coding

---
+++ crud/index.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown-options");
+    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+}
+
+const name = document.getElementById("name");
+const address = document.getElementById("address");
+const gender = document.getElementById("gender");
+const age = document.getElementById("age");
+const interests = Array.from(document.querySelectorAll(''))
