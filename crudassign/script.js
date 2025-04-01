98c80b5e11c1029f1413ae00bd57d5a827f9cf4e
Sweta Patel <sweta0442@gmail.com>
2025-04-01 23:30:18 +0530

    added api call restriction

--- crudassign/script.js
+++ crudassign/script.js
@@ -1,63 +1,63 @@
 const api = "https://jsonplaceholder.typicode.com/todos";
-
 const btn = document.getElementById("fetch");
 
+let isFetched = false;
+let div1; 
+
 btn.addEventListener("click", async () => {
-  let isLoading = true;
-  let loader ;
-  if (isLoading === true) {
-    loader = document.createElement("span");
-    loader.textContent = "Loading...";
-    document.body.appendChild(loader);
-  }
+  if (isFetched) return; 
+  
+  let loader = document.createElement("span");
+  loader.textContent = "Loading...";
+  document.body.appendChild(loader);
+
   const res = await fetch(api);
   const data = await res.json();
-  if (data) {
-    isLoading = false;
-    loader.style.display = 'none';
-    const div1 = document.createElement("div");
-    for (let i = 0; i < 10; i++) {
-      const list = document.createElement("li");
+  
+  loader.style.display = "none";
+  isFetched = true; 
 
-      const textSpan = document.createElement("span");
-      textSpan.textContent = data[i]?.title;
+  div1 = document.createElement("div"); 
 
-      //input field hidden initially
-      const input = document.createElement("input");
-      input.type = "text";
-      input.value = data[i]?.title;
-      input.style.display = 'none';
+  for (let i = 0; i < 10; i++) {
+    const list = document.createElement("li");
 
-      //edit button
-      const editBtn = document.createElement("button");
-      editBtn.textContent = "Edit";
+    const textSpan = document.createElement("span");
+    textSpan.textContent = data[i]?.title;
 
-      //delete btn
-      const delBtn = document.createElement("button");
-      delBtn.textContent = "Delete";
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = data[i]?.title;
+    input.style.display = "none";
 
-      editBtn.addEventListener("click", () => {
-        if(editBtn.textContent === 'Edit'){
-            textSpan.style.display = "none";
-            input.style.display = 'inline';
-            editBtn.textContent = 'Update';
-        }else {
-            textSpan.textContent = input.value;
-            textSpan.style.display = 'inline';
-            input.style.display = 'none';
-            editBtn.textContent = 'Edit'
-        }
-      });
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
 
-      delBtn.addEventListener("click", () => {
-        list.remove();
-      });
-      list.appendChild(textSpan);
-      list.appendChild(input);
-      list.appendChild(editBtn);
-      list.appendChild(delBtn);
-      div1.appendChild(list);
-    }
-    document.body.appendChild(div1);
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+
+    editBtn.addEventListener("click", () => {
+      if (editBtn.textContent === "Edit") {
+        textSpan.style.display = "none";
+        input.style.display = "inline";
+        editBtn.textContent = "Update";
+      } else {
+        textSpan.textContent = input.value;
+        textSpan.style.display = "inline";
+        input.style.display = "none";
+        editBtn.textContent = "Edit";
+      }
+    });
+
+    delBtn.addEventListener("click", () => {
+      list.remove();
+    });
+
+    list.appendChild(textSpan);
+    list.appendChild(input);
+    list.appendChild(editBtn);
+    list.appendChild(delBtn);
+    div1.appendChild(list);
   }
+  document.body.appendChild(div1);
 });
